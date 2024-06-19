1c2aef3c128a5e82a63c7c1f012d6b387cd2e57c
于勇涛 <smallbadoncelaugh@gmail.com>
2024-06-19 20:50:22 +0800

    Initial Commit

---
+++ GitVersionControl/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  GitVersionControl
+//
+//  Created by V™️ on 2024/6/19.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
